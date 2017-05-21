e3fe8440a9f2fd3e5f7a998305a70512eb833871
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2017-05-20 21:53:11 -0300

    chore(*) missing unicode literals

--- clearly/__init__.py
+++ clearly/__init__.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from __future__ import unicode_literals
 
 __author__ = 'Rogério Sampaio de Almeida'
 
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+from __future__ import unicode_literals
+
 from distutils.core import setup
 
 from setuptools import find_packages
