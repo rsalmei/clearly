19c3103bb122750eb76336bcc886f2bf7b7e0a74
Rogério <rsalmei@gmail.com>
2018-07-12 03:14:23 -0300

    test(examples) expand examples

--- tests/examples/tasks.py
+++ tests/examples/tasks.py
@@ -3,21 +3,29 @@ from celery import Celery
 app = Celery('tasks', broker='amqp://localhost', backend='redis://localhost')
 
 
-@app.task(bind=True)
+@app.task(bind=True, max_retries=20)
 def function_value(self, retries, **kwargs):
+    print(vars(self.request))
     if retries > self.request.retries:
-        raise self.retry(countdown=0, exc='retry-{}'.format(self.request.retries))
+        raise self.retry(countdown=1)
     return kwargs.get('value', -1)
 
 
 @app.task(bind=True)
 def function_kwargs(self, retries, **kwargs):
     if retries > self.request.retries:
-        raise self.retry(countdown=0, exc='retry-{}'.format(self.request.retries))
+        raise self.retry(countdown=1)
     return kwargs
 
 
 @app.task(bind=True)
 def function_none(self, retries, **kwargs):
     if retries > self.request.retries:
-        raise self.retry(countdown=0, exc='retry-{}'.format(self.request.retries))
+        raise self.retry(countdown=1)
+
+
+@app.task(bind=True, ignore_result=True)
+def function_ignore(self, retries, **kwargs):
+    if retries > self.request.retries:
+        raise self.retry(countdown=1)
+    return kwargs
