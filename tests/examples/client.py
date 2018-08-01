2b2a8366bcd77e90f59e3a9f559ad7ac2eef031b
Rogério <rsalmei@gmail.com>
2018-07-31 22:52:36 -0300

    chore(examples) new clearlycli constructor (do not uses app anymore)

--- tests/examples/client.py
+++ tests/examples/client.py
@@ -1,8 +1,5 @@
 from clearly.client import ClearlyClient
-from clearly.server import ClearlyServer
-from tasks import app
 
-srv = ClearlyServer(app)
-cli = ClearlyClient(srv)
+cli = ClearlyClient()
 
 cli.capture(params=True, success=True)
