3175a64af6bf57bf82f8c475f7cbb96153dab486
Rogério <rsalmei@gmail.com>
2018-08-01 01:21:09 -0300

    chore(expected_state) ensures states are always strings

--- clearly/expected_state.py
+++ clearly/expected_state.py
@@ -47,6 +47,7 @@ class ExpectedStateHandler(object):
 
 class ExpectedPath(object):
     def __init__(self, name):
+        assert isinstance(name, str)
         self.name = name
         self.possibles = ()
         self.default = None
