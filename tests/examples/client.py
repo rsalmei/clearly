25a98e23f4c075f9c35bcd2366c1348e6ea38a27
Rogério <rsalmei@gmail.com>
2018-08-28 17:12:07 -0300

    test(examples) include internal server test

--- tests/examples/client.py
+++ tests/examples/client.py
@@ -1,4 +1,8 @@
 from clearly.client import ClearlyClient
+from clearly.server import start_server
+
+# uncomment to test internal server
+# server = start_server('amqp://localhost', blocking=False)
 
 cli = ClearlyClient()
 
