3ba992f5c79248fbcf4418e0e70f0d3e75c7ffed
Rogério <rsalmei@gmail.com>
2020-03-13 01:59:06 -0300

    hide implementation detail

--- clearly/server.py
+++ clearly/server.py
@@ -15,8 +15,6 @@ from .protos import clearly_pb2, clearly_pb2_grpc
 from .utils.colors import Colors
 from .utils.data import accepts
 
-ONE_DAY_IN_SECONDS = 24 * 60 * 60
-
 logger = logging.getLogger('clearly.server')
 
 PATTERN_PARAMS_OP = operator.attrgetter('pattern', 'negate')
@@ -219,9 +217,10 @@ def _serve(instance, port, blocking):  # pragma: no cover
     if not blocking:
         return gserver
 
+    one_day_in_seconds = 24 * 60 * 60
     import time
     try:
         while True:
-            time.sleep(ONE_DAY_IN_SECONDS)
+            time.sleep(one_day_in_seconds)
     except KeyboardInterrupt:
         gserver.stop(None)
