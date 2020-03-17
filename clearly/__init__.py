ce1c9dd9eed636b5dae792cc20d4d5363169fd99
Rogério <rsalmei@gmail.com>
2020-03-17 19:56:56 -0300

    bump version

--- clearly/__init__.py
+++ clearly/__init__.py
@@ -1,7 +1,7 @@
-VERSION = (0, 7, 1)
+VERSION = (0, 8, 0)
 
 __author__ = 'Rogério Sampaio de Almeida'
 __email__ = 'rsalmei@gmail.com'
 __version__ = '.'.join(map(str, VERSION))
 
-__all__ = ('__author__', '__version__',)
+__all__ = ('__author__', '__version__', '__email__')
