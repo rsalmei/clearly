4d8cb91f4df11117f625e932cad2ab9e471e85cf
Rogério <rsalmei@gmail.com>
2020-03-18 06:55:40 -0300

    include python 3.8 compatibility

--- setup.py
+++ setup.py
@@ -54,12 +54,10 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.2',
-        # 'Programming Language :: Python :: 3.3',
-        # 'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     keywords='celery task queue job flower monitoring distributed asynchronous'.split(),
     packages=find_packages(),
