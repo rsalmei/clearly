4dac6f05f2b38ec42f32c854d85d718ceea1c96d
Rogério <rsalmei@gmail.com>
2020-03-25 16:27:52 -0300

    test logo module

--- tests/unit/utils/test_logo.py
+++ tests/unit/utils/test_logo.py
@@ -9,7 +9,5 @@ from clearly.utils import logo
     'client', 'server'
 ])
 def test_logo_has_version(system):
-    with mock.patch('clearly.__init__') as mock_init:
-        mock_init.__version__ = 'cool'
-
+    with mock.patch('clearly.utils.logo.__version__', 'cool'):
         assert 'cool' in logo.render(system)
