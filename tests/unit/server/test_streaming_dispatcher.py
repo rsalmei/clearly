42f02818a49315b8945edf00a25773c65dffb9c0
Rogério <rsalmei@gmail.com>
2020-03-30 15:09:37 -0300

    final tests for streaming_dispatcher

--- tests/unit/server/test_streaming_dispatcher.py
+++ tests/unit/server/test_streaming_dispatcher.py
@@ -1,82 +1,70 @@
 import re
 from queue import Queue
 from unittest import mock
+from unittest.mock import DEFAULT, call
 
 import pytest
 
-from clearly.event_core.events import TaskData, WorkerData
-from clearly.event_core.streaming_dispatcher import CapturingClient, StreamingDispatcher
+from clearly.protos.clearly_pb2 import TaskMessage, WorkerMessage
+from clearly.protos.clearly_pb2 import PatternFilter
+from clearly.server.streaming_dispatcher import StreamingDispatcher, Role
+
+
+@pytest.fixture(params=Role.__members__.values())
+def dispatcher(request):
+    with mock.patch('threading.Thread'):
+        yield StreamingDispatcher(Queue(), request.param)
+
+
+def test_dispatcher_new_streaming_client(dispatcher, bool1):
+    q = Queue()
+    with dispatcher.streaming_capture(PatternFilter(pattern='regex', negate=bool1), q):
+        assert dispatcher.observers[0] == (q, re.compile('regex'), bool1)
+    assert dispatcher.observers == []
+
+
+def test_dispatcher_streaming_client_dont_leak(dispatcher):
+    q = Queue()
+    try:
+        with dispatcher.streaming_capture(PatternFilter(pattern='regex', negate=False), q):
+            1 / 0
+    except:
+        pass
+    assert dispatcher.observers == []
 
 
 @pytest.fixture
-def dispatcher():
-    with mock.patch('threading.Thread'):
-        yield StreamingDispatcher(Queue())
+def task_message():
+    with mock.patch('clearly.utils.data.TASK_OP'):
+        yield TaskMessage(name='name', routing_key='routing_key', uuid='uuid', retries=5,
+                          args='args', kwargs='kwargs', result='result', traceback='traceback',
+                          timestamp=123.1, state='state')
 
 
-@pytest.fixture(params=(False, True))
-def bool1(request):
-    return request.param
+@pytest.fixture
+def worker_message():
+    with mock.patch('clearly.utils.data.WORKER_OP'):
+        yield WorkerMessage(hostname='hostname', pid=12000, sw_sys='sw_sys', sw_ident='sw_ident',
+                            sw_ver='sw_ver', loadavg=[1., 2., 3.], processed=123,
+                            freq=5, timestamp=123.1, state='state', heartbeats=[1])
 
 
-@pytest.fixture(params=(False, True))
-def bool2(request):
-    return request.param
-
-
-def test_dispatcher_new_streaming_client(dispatcher, bool1, bool2):
-    with dispatcher.streaming_client('tasks_regex', bool1,
-                                     'workers_regex', bool2) as q_client:
-        assert dispatcher.observers[-1].queue == q_client
-        assert dispatcher.observers[-1].task_regex == re.compile('tasks_regex')
-        assert dispatcher.observers[-1].worker_regex == re.compile('workers_regex')
-        assert dispatcher.observers[-1].task_negate == bool1
-        assert dispatcher.observers[-1].worker_negate == bool2
-
-
-@pytest.mark.parametrize('event_data, path, change, accepts_params', [
-    (TaskData(name='name', routing_key='routing_key', uuid='uuid', retries=5,
-              args='args', kwargs='kwargs', result='result', traceback='traceback',
-              timestamp=123.1, state='state', pre_state='other', created=False),
-     'task_states', dict(uuid='roger'), ('task_regexp', 35, 'name', 'routing_key')),
-    (WorkerData(hostname='hostname', pid=12000, sw_sys='sw_sys', sw_ident='sw_ident',
-                sw_ver='sw_ver', loadavg='loadavg', processed='processed',
-                alive=True, freq=5, last_heartbeat=1, state='state', pre_state='other',
-                created=False),
-     'worker_states', dict(pid=6700), ('worker_regexp', 47, 'hostname')),
-])
-def test_dispatcher_dispatch(event_data, path, change, accepts_params, bool1, dispatcher):
+def test_dispatcher_dispatch(task_message, worker_message, dispatcher):
+    from clearly.utils.data import TASK_OP, WORKER_OP
+    message, op = (task_message, TASK_OP) if dispatcher.role == Role.TASKS \
+        else (worker_message, WORKER_OP)
     q = Queue()
-    cc = CapturingClient(q, 'task_regexp', 35, 'worker_regexp', 47)
-    dispatcher.observers.append(cc)
+    observer = q, 'compiled', False
+    dispatcher.observers.append(observer)
 
-    with mock.patch(
-            'clearly.event_core.streaming_dispatcher.StreamingDispatcher.generate_states'
-    ) as gs, mock.patch('clearly.event_core.streaming_dispatcher.accepts') as acc:
-        gs.return_value = (change,)
-        acc.return_value = bool1
-
+    with mock.patch('clearly.utils.data._accept') as mock_accept:
+        mock_accept.return_value = False
         # noinspection PyProtectedMember
-        dispatcher._dispatch(event_data)
+        dispatcher._dispatch('not cool')
 
-    acc.assert_called_once_with(*accepts_params)
-    if bool1:  # accepted or not
-        gs.assert_called_once_with(event_data, getattr(dispatcher, path))
-        to_client = q.get_nowait()
-        assert all(getattr(to_client, k) == v for k, v in change.items())
-    else:
-        gs.assert_not_called()
+        mock_accept.return_value = True
+        # noinspection PyProtectedMember
+        dispatcher._dispatch(message)
 
-
-def test_dispatcher_generate_states(bool1, dispatcher):
-    event_data = TaskData(created=bool1, name='name', routing_key='routing_key', uuid='uuid',
-                          retries=5, args='args', kwargs='kwargs', result='result',
-                          traceback='traceback', timestamp=123.1, state='state', pre_state='other')
-    with mock.patch.object(dispatcher.task_states, 'states_through') as st:
-        st.return_value = ('middle',)
-        gen = StreamingDispatcher.generate_states(event_data, dispatcher.task_states)
-        if bool1:
-            assert next(gen)['state'] == '-'
-        assert next(gen)['state'] == 'middle'
-        with pytest.raises(StopIteration):
-            next(gen)
+    assert mock_accept.call_args == call('compiled', False, op())
+    assert q.get_nowait() == message
