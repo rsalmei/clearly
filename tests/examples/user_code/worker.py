7cef6dba5051fe8afbf5502be73155ea9797857f
Rogério <rsalmei@gmail.com>
2020-04-08 05:11:39 -0300

    example worker trying to not truncate, but unfortunately it isn't working

--- tests/examples/user_code/worker.py
+++ tests/examples/user_code/worker.py
@@ -4,6 +4,9 @@ app = Celery('tasks', broker='amqp://localhost', backend='redis://localhost')
 app.conf.task_send_sent_event = True
 app.conf.task_serializer = 'pickle'  # to be able to demo all internal compiler goodies.
 app.conf.accept_content = ['pickle', 'json']  # apparently canvas workflow are always json.
+app.conf.resultrepr_maxsize = 100 * 1024  # to not truncate args and kwargs until that size.
+# unfortunately it is not working, as of 4.4.2
+# http://docs.celeryproject.org/en/latest/reference/celery.app.task.html#celery.app.task.Task.resultrepr_maxsize
 
 
 @app.task(bind=True, max_retries=3)
