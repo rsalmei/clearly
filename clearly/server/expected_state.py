26c4175232bf66af486ddff05b6e741be193dbe1
Rogério <rsalmei@gmail.com>
2020-04-08 00:31:10 -0300

    move up class to enable better annotations

--- clearly/server/expected_state.py
+++ clearly/server/expected_state.py
@@ -3,6 +3,34 @@ from typing import Tuple, Iterable, Optional, Union
 from celery import states as task_states
 
 
+class ExpectedPath:
+    def __init__(self, name: str):
+        self.name = name
+        self.possibles = ()
+        self.default = None
+
+    def to(self, names: Union[str, 'ExpectedPath', Tuple[Union[str, 'ExpectedPath'], ...]],
+           default: Optional[str] = None) -> 'ExpectedPath':
+        if not isinstance(names, tuple):
+            names = (names,)
+        paths = tuple(ExpectedPath(name) if isinstance(name, str) else name
+                      for name in names)
+        self.possibles = (self.possibles or ()) + paths
+        if default:
+            self.default = ExpectedPath(default) if isinstance(default, str) else default
+        return paths[0] if len(names) == 1 else self.default
+
+    def find(self, name: str) -> 'ExpectedPath':
+        for n in self.possibles:
+            if n.name == name:
+                return n
+        if len(self.possibles) == 1:
+            return self.possibles[0]
+        if self.default:
+            return self.default
+        raise UserWarning('unknown name {} in {}'.format(name, self.name))
+
+
 class ExpectedStateHandler:
     """Events received will usually be totally disordered, which is not a
     problem for the final state, as celery itself takes into account their
@@ -36,34 +64,6 @@ class ExpectedStateHandler:
 
         pointer = yield from self.__traverse(self.expected_path, pre, False)
         yield from self.__traverse(pointer, post, True)
-
-
-class ExpectedPath:
-    def __init__(self, name: str):
-        self.name = name
-        self.possibles = ()
-        self.default = None
-
-    def to(self, names: Union[str, 'ExpectedPath', Tuple[Union[str, 'ExpectedPath'], ...]],
-           default: Optional[str] = None) -> 'ExpectedPath':
-        if not isinstance(names, tuple):
-            names = (names,)
-        paths = tuple(ExpectedPath(name) if isinstance(name, str) else name
-                      for name in names)
-        self.possibles = (self.possibles or ()) + paths
-        if default:
-            self.default = ExpectedPath(default) if isinstance(default, str) else default
-        return paths[0] if len(names) == 1 else self.default
-
-    def find(self, name: str) -> 'ExpectedPath':
-        for n in self.possibles:
-            if n.name == name:
-                return n
-        if len(self.possibles) == 1:
-            return self.possibles[0]
-        if self.default:
-            return self.default
-        raise UserWarning('unknown name {} in {}'.format(name, self.name))
 
 
 def setup_task_states() -> ExpectedStateHandler:
