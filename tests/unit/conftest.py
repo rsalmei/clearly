03c955cc354c7afbffbcc43c6894cb311d90f867
Rogério <rsalmei@gmail.com>
2020-04-08 05:13:44 -0300

    new fixture to streamline even more tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -13,13 +13,19 @@ def task_state_type(request):
     yield request.param
 
 
-@pytest.fixture(params=sorted(task_states.ALL_STATES.union(set('?'))))
-def task_state_plus(request):
+@pytest.fixture(params=sorted(task_states.ALL_STATES.union({'?'})))
+def task_state_plus_1(request):
     """Can be used for a double SUCCESS, which truncates results or not, or for a
     double PENDING, which is created or not."""
     # task results only make sense in success, so to test truncate resolving mechanism,
     # I can't just insert a bool, as almost all combinations would not make sense.
     # the same for pending.
+    yield request.param
+
+
+@pytest.fixture(params=sorted(task_states.ALL_STATES.union({'?1', '?2'})))
+def task_state_plus_2(request):
+    """Same as above, but with 2 additional states."""
     yield request.param
 
 
