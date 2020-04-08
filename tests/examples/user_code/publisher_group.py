600e68fc3e4b708090f5c3349d002ea9c3d2fbf8
Rogério <rsalmei@gmail.com>
2020-04-08 00:23:20 -0300

    improve examples group

--- tests/examples/user_code/publisher_group.py
+++ tests/examples/user_code/publisher_group.py
@@ -2,11 +2,11 @@ import time
 
 from celery import chord, group
 
-from .tasks import *
+from .worker import function_aggregate, function_test
 
 chord(
-    group(function_value.s(0, value=i) for i in range(1000)),
-    function_any.s(from_chord=True)
+    group(function_test.s(0, value=i) for i in range(1000)),
+    function_aggregate.s(from_chord=True)
 )()
 
 time.sleep(5)
